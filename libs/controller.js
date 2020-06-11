2d8f428f3619a873053f8ef0445c3d692a7a21f0
Анатолий Фащевский <reals@iRealsMac.local>
2020-06-11 12:27:38 +0300

    change serialport speed

--- libs/controller.js
+++ libs/controller.js
@@ -161,7 +161,7 @@ async function getReaders() {
             let relay = null;
             if (record.type == "RS232") {
                 try {
-                    port = new SerialPort(record.port, { baudRate: 9600 });
+                    port = new SerialPort(record.port, { baudRate: 115200 });
                 } catch (err) {}
             }
             if (record.type == "Wiegand") {
