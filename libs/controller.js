7e1d3a085e402915e0387e51fb34332dd4e88b35
Анатолий Фащевский <reals@iRealsMac.local>
2020-06-11 16:24:11 +0300

    test

--- libs/controller.js
+++ libs/controller.js
@@ -204,8 +204,11 @@ module.exports = async function () {
                 reader.port.on("data", async (chunk) => {
                     buffer += chunk.toString();
                     var buffer_arr = buffer.split(/\r?\n/);
-                    var number = buffer_arr[0];
-                    if (
+                    var number = buffer_arr[0].replace(
+                        /[\u{0001}-\u{0007}]/gu,
+                        ""
+                    );
+                    /* if (
                         number.charCodeAt(0) === 0x0001 ||
                         number.charCodeAt(0) === 0x0002 ||
                         number.charCodeAt(0) === 0x0003 ||
@@ -220,7 +223,7 @@ module.exports = async function () {
                         number.charCodeAt(number.length - 1) === 0x0004
                     ) {
                         number = number.slice(0, -1);
-                    }
+                    } */
                     console.log(number, number.length);
                     if (buffer_arr.length > 0 && number.length == 10) {
                         //number = buffer.slice(1, 11);
